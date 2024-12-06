f370adc6fbcb3353f8e7e27f3a84b56ba892a4f7
liuxiaobo <1294605662@qq.com>
2024-12-06 11:52:59 +0800

    RM ID：12/6

---
+++ ANTLR/src/test/java/org/example/antlr/AntlrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.antlr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntlrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
